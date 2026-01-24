0294c490dfe556194f322afb64910b7ba22cd9d2
Amy <amy.northway@warwick.ac.uk>
2026-01-24 16:22:59 +0000

    began grid eval

--- grid.py
+++ grid.py
@@ -13,7 +13,7 @@ def getSquare(x: np.array, grid: pd.DataFrame):
 
 # M is the number of squares to split grid into, d is the dimension, e is epsilon
 # assume the data is normalised so each dimension is in [-1,1]
-def create_grid_synopsis(X: np.ndarray, e: float, d: int, M: Optional[float] = None) -> np.ndarray:
+def create_grid_synopsis(X: np.ndarray, e: float, d: int, M: Optional[float] = None, seed=42) -> np.ndarray:
 
     # a non-private approximation for M. Ideally M would be given. 
     if not M:
@@ -34,14 +34,14 @@ def create_grid_synopsis(X: np.ndarray, e: float, d: int, M: Optional[float] = N
     counts = np.bincount(assigned, minlength=grid.shape[0]).astype(float)
 
     # here we would add laplace noise to the counts, since the l1 norm of the counts vector is 1 we want to add a vector sampled from lap(1/e)
-    counts += noise(1 / e, grid.shape[0])
+    counts += noise(1 / e, grid.shape[0], seed)
 
     # combine the grid coordinate with the counts 
     synopsis = np.hstack([grid, counts[:, None]])
 
     return synopsis
 
-
+"""
 rng = np.random.default_rng(42)
 X1 = rng.multivariate_normal(mean=(0.5,0.5), cov=[[1,0],[0,1]], size=200)
 X2 = rng.multivariate_normal(mean=(-0.5,-0.5), cov=[[1,0],[0,1]], size=150)
@@ -51,3 +51,4 @@ plt.scatter(x=p.iloc[:, 0], y=p.iloc[:, 1], s=p.iloc[:, 2])
 plt.show()
 print(p)
 print(lloyd_with_weights(2, p.iloc[:, :-1], p.iloc[:, -1], n_iter=5))
+"""
