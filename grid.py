a2700e3b29d56262b46a8df0494eab6661867d96
Amy <amy.northway@warwick.ac.uk>
2026-01-24 15:20:34 +0000

    change to numpy

--- grid.py
+++ grid.py
@@ -13,7 +13,7 @@ def getSquare(x: np.array, grid: pd.DataFrame):
 
 # M is the number of squares to split grid into, d is the dimension, e is epsilon
 # assume the data is normalised so each dimension is in [-1,1]
-def create_grid_synopsis(X: pd.DataFrame, e: float, d: int, M: Optional[float] = None) -> pd.DataFrame:
+def create_grid_synopsis(X: np.ndarray, e: float, d: int, M: Optional[float] = None) -> np.ndarray:
 
     # a non-private approximation for M. Ideally M would be given. 
     if not M:
@@ -23,26 +23,32 @@ def create_grid_synopsis(X: pd.DataFrame, e: float, d: int, M: Optional[float] =
     # create the grid 
     edges = np.linspace(-1, 1, M + 1)
     centers = (edges[:-1] + edges[1:]) / 2
-    grid = pd.DataFrame(np.array(list(itertools.product(centers, repeat=d))))
+    grid = np.array(list(itertools.product(centers, repeat=d)))
 
     # assign each point to a square in the grid 
-    squares = X.apply(lambda row: getSquare(row, grid), axis=1)
+    diffs = X[:, None, :] - grid[None, :, :]
+    dists = np.linalg.norm(diffs, axis=2)
+    assigned = np.argmin(dists, axis=1)
 
     # get counts of points in the grid, used for the weights 
-    points = squares.value_counts().reset_index(name="count")
-    all_points = pd.merge(points, grid, on = list(grid.columns),  how="outer").fillna(0)
+    counts = np.bincount(assigned, minlength=grid.shape[0]).astype(float)
+
     # here we would add laplace noise to the counts, since the l1 norm of the counts vector is 1 we want to add a vector sampled from lap(1/e)
-    all_points["count"] += noise(1/e, grid.shape[0])
-    return all_points
+    counts += noise(1 / e, grid.shape[0])
+
+    # combine the grid coordinate with the counts 
+    synopsis = np.hstack([grid, counts[:, None]])
+
+    return synopsis
 
 
-"""
-X1 = pd.DataFrame(np.random.multivariate_normal(mean=(5,10), cov=[[5,0],[0,5]], size=200))
-X2 = pd.DataFrame(np.random.multivariate_normal(mean=(2,3), cov=[[5,0],[0,5]], size=150))
-X = normalise(pd.concat([X1, X2]))
-p = create_grid_synopsis(X, 1, 2)
+
+X1 = np.random.multivariate_normal(mean=(5,10), cov=[[5,0],[0,5]], size=200)
+X2 = np.random.multivariate_normal(mean=(2,3), cov=[[5,0],[0,5]], size=150)
+X = np.concat([X1, X2])
+X = X - X.mean() 
+p = pd.DataFrame(create_grid_synopsis(X, 1, 2))
 plt.scatter(x=p.iloc[:, 0], y=p.iloc[:, 1], s=p.iloc[:, 2])
 plt.show()
 print(p)
 print(lloyd_with_weights(2, p.iloc[:, :-1], p.iloc[:, -1], n_iter=5))
-"""
